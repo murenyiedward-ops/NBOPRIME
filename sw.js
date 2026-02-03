e1830408a2e139956ccc6fccc6b586f0bb2ad4c0
murenyiedward-ops <murenyiedward@gmail.com>
2026-02-03 15:24:07 +0300

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,15 +1,22 @@
 const CACHE_NAME = 'np-v1';
 const ASSETS = [
-  './',
-  './index.html',
-  './merchant.html',
-  './admin.html'
+  '/NBOPRIME/',
+  '/NBOPRIME/index.html',
+  '/NBOPRIME/partnrr.html',
+  '/NBOPRIME/admin.html',
+  '/NBOPRIME/manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
